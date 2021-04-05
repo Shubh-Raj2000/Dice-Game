3b2bb3ef6772f592d3e5b4a26fca3e8c33eedb01
Shubh-Raj2000 <shubhamraj1099@gmail.com>
2021-04-05 17:48:49 +0530

    initial dice game files

---
+++ dicee.js
@@ -0,0 +1,14 @@
+var randomNumber1,randomNumber2;
+randomNumber1=Math.floor(Math.random()*6)+1;
+randomNumber2=Math.floor(Math.random()*6)+1;
+document.querySelector(".img1").setAttribute("src", "images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src", "images/dice"+randomNumber2+".png");
+if (randomNumber1>randomNumber2) {
+  document.querySelector("h1").textContent="🚩Player 1 Wins!";
+}
+else if (randomNumber1<randomNumber2) {
+  document.querySelector("h1").textContent="Player 2 Wins!🚩";
+}
+else {
+  document.querySelector("h1").textContent="🚩Draw!🚩";
+}
